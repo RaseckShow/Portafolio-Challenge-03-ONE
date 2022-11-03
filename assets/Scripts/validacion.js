02e7cf45d7c5ea0aaed7eef88db7a152c3d2576a
RaseckShow <ces_art11@hotmail.com>
2022-11-02 20:48:18 -0600

    Implementacion de la funcion Validar Boton de Envio del Formulario

--- assets/Scripts/validacion.js
+++ assets/Scripts/validacion.js
@@ -1,9 +1,16 @@
-const boton = document.getElementById("btn-enviarFormulario");
+const formulario = document.getElementById("formulario");
 const inputs = document.querySelectorAll("input, textarea");
+
 const expresiones = {
   nombre: /^[a-zA-Z\s]{2,30}$/,
   email: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
   mensaje: /[\s\S]{10,180}/
+};
+
+const campos = {
+  nombre: false,
+  email: false,
+  mensaje: false
 };
 
 const validarFormulario = (e) =>{
@@ -24,15 +31,27 @@ const validarCampo = (expresion, input, campo) =>{
     document.getElementById(`input__container__${campo}`).classList.remove("input__container-invalido");
     document.getElementById(`input__container__${campo}`).classList.add("input__container-valido");
     document.getElementById(`${campo}-error`).style.display = "none";
-    console.log(campo);
+    campos[campo] = true;
   }else{
     document.getElementById(`input__container__${campo}`).classList.remove("input__container-valido");
     document.getElementById(`input__container__${campo}`).classList.add("input__container-invalido");
     document.getElementById(`${campo}-error`).style.display = "flex";
-    console.log(campo);
+    campos[campo] = false;
   }
 }
 
 inputs.forEach((input) => {
   input.addEventListener("blur",validarFormulario)
+});
+
+formulario.addEventListener("submit", (e) =>{
+  e.preventDefault();
+  if(campos.nombre && campos.email && campos.mensaje){
+    formulario.reset();
+  
+    document.getElementById("mensaje__envioCorrecto").classList.add("mensaje__envioCorrecto-activo");
+    setTimeout(() =>{
+      document.getElementById("mensaje__envioCorrecto").classList.remove("mensaje__envioCorrecto-activo");
+    }, 2500);
+  }
 });
